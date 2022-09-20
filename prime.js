7c472455bfb2adb6adf01c1083373fea38d05505
themanvendra <manvendra.singh@ssipmt.com>
2022-09-20 18:25:59 +0530

    code fixed

--- prime.js
+++ prime.js
@@ -1,14 +1,13 @@
 // Check whether a number is Prime or not.
 let number = 13;
-let factor=0;
+let flag=false;
 for (let i =0 ; i <= number; i++) {
   if (number % i == 0) {
-   
-   factor++
+   flag=true;
   
   }
 }
-if (factor==2) {
+if (flag==true) {
   console.log(number, "is a prime number.")
 } else {
   console.log(number, "is not a prime number.")
